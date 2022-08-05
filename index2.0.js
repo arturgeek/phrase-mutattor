091c6acf7f310209a17eaa47443d4657389da600
Andres Morales <andres.morales@impartner.com>
2022-08-05 11:11:05 -0500

    review and clean code

--- index2.0.js
+++ index2.0.js
@@ -8,40 +8,29 @@ Words are separated by spaces or non-alphabetic characters and these separators 
 A few of the things we will be looking at is accuracy, efficiency, solution completeness. 
 */
 
+
 const WORDS_REGEX = /\W/g;
 const SEPARATORS_REGEX = /([a-zA-Z])+/g;
 
 const getWords = ( string ) => {
     let words = [...string.matchAll(SEPARATORS_REGEX)].map( matched => {return matched[0] });
     words = words.filter( word => word.length > 2 && word != " " && word != "" );
-    //console.log(words);
-    
     return words;
 }
 
 const getShrinkedWords = (words) => {
     return words.map( word => {
         return shrinkWord(word);
-    })
+    });
 };
 
 const shrinkWord = (word) => {
     let shrinked = word.split("");
     let first = shrinked.shift();
     let last = shrinked.pop();
+    let length = [...new Set(shrinked)].length;
 
-    let length = "";
-    if( word.length > 2 ) {
-        length = [...new Set(shrinked)].length;
-    }
-
-    shrinked = first + length + last;    
-    return shrinked;
-}
-
-const buildResult = ( words, separators ) => {
-    let sentence = words.map( (word, index) => word + separators[index] ).join("");
-    return sentence;
+    return first + length + last;
 }
 
 function wordParser( string ) {
@@ -49,12 +38,10 @@ function wordParser( string ) {
     let words = getWords(string);
     let shrinkedWords = getShrinkedWords(words);
 
-    //console.log(words);
-    //console.log(shrinkedWords);
+    console.log(words);
+    console.log(shrinkedWords);
 
     words.forEach( (word, index) => {
-        //console.log(string);
-        //string = string.replace( new RegExp(word, 'g'), shrinkedWords[index] ) 
         string = string.replace( word, shrinkedWords[index] ) 
     });
     
@@ -63,8 +50,10 @@ function wordParser( string ) {
 
 console.log("-".repeat(50))
 
-var output = wordParser("Creativity is thinking-up new things. Innovation is doing new things!");
+var phrase = "Creativity is thinking-up new things. Innovation is doing new things!";
+var output = wordParser(phrase);
 let result = "C6y is t4g-up n1w t4s. I6n is d3g n1w t4s!";
+////expected: C6y is t4g-up n1w t4s. I6n is d3g n1w t4s!
 
 console.log(output);
 console.log(result);
@@ -72,7 +61,8 @@ console.log( output === result )
 
 console.log("-".repeat(50))
 
-output = wordParser("||Creativity is thinking-up new things since 99. Innovation is doing new things!!!");
+phrase = "||Creativity is thinking-up new things since 99. Innovation is doing new things!!!";
+output = wordParser(phrase);
 result = "||C6y is t4g-up n1w t4s s3e 99. I6n is d3g n1w t4s!!!";
 
 console.log(output);
@@ -81,7 +71,8 @@ console.log( output === result )
 
 console.log("-".repeat(50))
 
-output = wordParser("!i want to ch3ck ThiS pa55worD right Now--//");
+phrase = "!i want to ch3ck ThiS pa55worD right Now--//";
+output = wordParser(phrase);
 result = "!i w2t to ch3ck T2S pa55w2D r3t N1w--//";
 
 console.log(output);
@@ -90,7 +81,8 @@ console.log( output === result )
 
 console.log("-".repeat(50))
 
-output = wordParser("1a 2ab 3abc ...[]... 15abcdefgh... quatre20dix '''");
+phrase = "1a 2ab 3abc ...[]... 15abcdefgh... quatre20dix '''";
+output = wordParser(phrase);
 result = "1a 2ab 3a1c ...[]... 15a6h... q4e20d1x '''";
 
 console.log(output);
@@ -99,4 +91,17 @@ console.log( output === result )
 
 console.log("-".repeat(50))
 
+phrase = "andres arturo morales";
+output = wordParser(phrase);
+result = "a4s a3o m5s";
+
+console.log(output);
+console.log(result);
+console.log( output === result )
+
+console.log("-".repeat(50))
+// expected: C6y is t4g-up n1w t4s. I6n is d3g n1w t4s!
+
+
 // Your last JavaScript (Node) code is saved below:
+
